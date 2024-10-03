1839928a29b0c65db9c6471eef6d6feabd460944
mg.apisara <111364885+mg-dotcom@users.noreply.github.com>
2024-10-04 00:03:06 +0700

    can get room types

--- BE-MusicRoomReservation-project/index.js
+++ BE-MusicRoomReservation-project/index.js
@@ -1,10 +1,13 @@
-const jsonServer = require("json-server"); // importing json-server library
+import jsonServer from "json-server"; // ES module import
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+const port = process.env.PORT || 8080; // Choose port from here, e.g., 8080 or 3001
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
